124f1ea0755277224b41ff994410d19b2a1c4dc0
gladpingvin42 <tr4zhm4il@gmail.com>
2015-05-07 17:25:37 +0200

    Simple implementation of MapManager

--- src/MapManager.java
+++ src/MapManager.java
@@ -1,4 +1,18 @@
 
 public class MapManager {
 
+	private static Room firstRoom;
+	
+	/**
+	 * Get the starting room in the game
+	 * @return The room the game starts in.
+	 */
+	public static Room getFirstRoom() {return firstRoom;}
+	
+	/**
+	 * Generate the map of the game.
+	 */
+	public static void generateMap() {
+		//ToDo: Put code to generate map here
+	}
 }

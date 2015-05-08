e5a1456267d06947bf995ae7f74ced0a60f01406
gladpingvin42 <tr4zhm4il@gmail.com>
2015-05-08 18:19:42 +0200

    Added movement code to Player
    Added movement code to class Player.

--- src/Player.java
+++ src/Player.java
@@ -6,6 +6,9 @@ import org.newdawn.slick.opengl.Texture;
 public class Player extends Entity {
 	private final String name;
 	private final Image image;
+	private float speed = 0.2f;
+	
+	private boolean[] directionsToMove = new boolean[4];
 	
 	/**
 	 * Create a new Player.
@@ -34,9 +37,28 @@ public class Player extends Entity {
 	public Image getImage() {return image;}
 	
 	/**
-	 * Handles player movement. 
+	 * Specify that the player should move in this direction on the next update.
+	 * @param direction An integer in the range 0-3 specifying which direction (up, right, down, left).
 	 */
-	public void move() {
+	public void move(int direction) {
+		if (direction > 3 || direction < 0) {throw new IllegalArgumentException("Direction must be between 0 and 3");}
+		directionsToMove[direction] = true;
+	}
+	
+	/**
+	 * {@inheritDoc Entity} 
+	 */
+	@Override
+	public void update() {
+		Point velocity = new Point(0,0);
 		
+		if (directionsToMove[0] && !directionsToMove[2]) { velocity.setY(speed);} //Up
+		if (directionsToMove[2] && !directionsToMove[0]) { velocity.setY(-speed);} //Down
+		
+		if (directionsToMove[3] && !directionsToMove[1]) { velocity.setX(-speed);} //Left
+		if (directionsToMove[1] && !directionsToMove[3]) { velocity.setX(speed);} //Right
+		
+		setVelocity(velocity);
+		super.update();
 	}
 }

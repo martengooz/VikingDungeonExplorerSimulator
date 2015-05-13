b6fd54e0f340967a604617202ebfe6146eb5bdef
martengooz <martengooz@gmail.com>
2015-05-13 17:44:20 +0200

    Changed to static class
    Changed UserInterfaceManager to a static class.

--- src/UserInterfaceManager.java
+++ src/UserInterfaceManager.java
@@ -1,23 +1,15 @@
 import org.newdawn.slick.Graphics;
 
 
-public class UserInterfaceManager {
-	private final Graphics g;
-	
-	/**
-	 * Create a new UserInterfaceManager for handling drawing UI.
-	 * @param g The Graphics context used by the container.
-	 */
-	public UserInterfaceManager(Graphics g) {
-		this.g = g;
-	}
+public class UserInterfaceManager {	
 	
 	/**
 	 * Show a message on screen. 
+	 * @param g The graphics context used by the container.
 	 * @param title The title of the message.
 	 * @param message The message to print. 
 	 */
-	public void showMessage(String title, String message) {	
+	public static void showMessage(Graphics g, String title, String message) {	
 		System.out.println("Title: " + title + ", Message: " + message);
 	}
 }
